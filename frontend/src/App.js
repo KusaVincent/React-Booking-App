be9e26adb96dad1b18de0132f9af1bcdccd20336
unknown <vincentkusa170203@daystar.ac.ke>
2020-02-10 00:05:21 +0300

    added navbar to my page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Redirect, Route, BrowserRouter, Switch } from "react-router-dom";
 import AuthPage from "./pages/Auth";
 import EventsPage from "./pages/events";
 import BookingsPage from "./pages/bookings";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 import "./App.css";
 
@@ -11,12 +12,17 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </>
       </BrowserRouter>
     );
   }
